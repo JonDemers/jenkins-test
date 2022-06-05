fcd79e8d00552ba06dc216db11376f603b1fd0ed
Jonathan Demers <jdemers@opcodesolutions.com>
2022-06-05 14:24:01 -0400

    HelloController v8

--- src/main/java/com/opcode/jenkins/api/HelloController.java
+++ src/main/java/com/opcode/jenkins/api/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello from Spring Boot v7 !!\n"
+		return "Hello from Spring Boot v8 !!\n"
 				+ "Date: " + new Date() + "\n";
 	}
 
