082e4605c58d1f7c5b39753ad6637695e60646e2
Jonathan Demers <jdemers@opcodesolutions.com>
2022-06-11 09:44:38 -0400

    Use Ingress instead of NodePort

--- src/main/java/com/opcode/jenkins/api/HelloController.java
+++ src/main/java/com/opcode/jenkins/api/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello from Spring Boot v8 !!\n"
+		return "Hello from Spring Boot v9 !!\n"
 				+ "Date: " + new Date() + "\n";
 	}
 
